46e147c69da0fb278c7ef1d69362a62e983c1fec
John Cordano <jecordano@yahoo.com>
2018-02-01 19:02:39 -0800

    Updated project with style changes.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Giphy-Project
+
+For this project, the user clicks buttons to populate the web page with animal images. The user can also enter the name of an animal in the text box, and then click the Submit button to add another button to the page.
+
+The html page is populated with animal images by using JavaScript and jQuery. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,15 @@
 
-// Creates initial array of animals.
+// Creates the initial array of animals.
 var animals = ["Cat", "Dog", "Horse", "Hamster"];
 
-// displayAnimalInfo function re-renders the HTML to display the appropriate content.
+// Re-renders the HTML to display the appropriate content.
 function displayAnimalInfo() {
   // Deletes the images prior to adding new images.
   $("#animals-view").empty();
-
+  // Creates a variable to use to retreive animal images from Giphy.
   var animal = $(this).attr("data-name");
-
+  // Extracts the animal images from Giphy.
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=cRJcC3cUgepwCr7h0LWKrpVX8bfYG9JD&limit=10";
-  
   // Creates an AJAX call for the clicked animal button.
   $.ajax({
     url: queryURL,
@@ -18,7 +17,6 @@ function displayAnimalInfo() {
   }).then(function(response) {
       // Creates a variable for the response data.
       var result = response.data;
-      
       // Loops through the response data to determine the ratings and
       // images to display. 
       for (var i = 0; i < result.length; i++) {
@@ -28,41 +26,40 @@ function displayAnimalInfo() {
           var image = $("<img>");
           // Stores the rating data.
           var rating = result[i].rating;
-          // Creates an element to have the rating displayed.
+          // Creates an element to display the rating.
           var myRating = $("<p>").text("Rating: " + rating);
-          // Displaying the rating.
+          // Appends the rating to the page.
           animalDiv.append(myRating);
           // Retrieves the URL for the still image.
           var imgURL = result[i].images.fixed_height_still.url;
-          // Assigns the still attribute URL to the image.
+          // Assigns the still attribute URL to the still image.
           image.attr("data-still", imgURL);
-          // Revtrieves the URL for the animated image.
+          // Retrieves the URL for the animated image.
           var imgGif = result[i].images.fixed_height.url;
-          // Assigns the animated attribute URL to the image.
+          // Assigns the animated attribute URL to the animated image.
           image.attr("data-animate", imgGif);
-          // Assigns the source attribute to URL the image. 
+          // Assigns the source attribute URL the animated image. 
           image.attr("src", imgURL);
-          // Assings the default state attribute to the image.
+          // Assigns the default state attribute to the still image.
           image.attr("data-state" , "still");
-          // Appends the image.
+          // Appends the image to the page.
           animalDiv.append(image);
-          // Puts the animal before the previous animals.
+          // Inserts the animal image before the previous animal images.
           $("#animals-view").prepend(animalDiv);
       }
     
   });
 }
 
-// Function for displaying animal images.
+// Displays the animal images.
 function renderButtons() {
 
   // Deletes the buttons prior to adding new buttons.
   $("#buttons-view").empty();
 
-  // Loops through the array of animals.
+  // Loops through the array of animal images.
   for (var i = 0; i < animals.length; i++) {
-
-    // Dynamicaly generates buttons for each animal in the array.
+    // Dynamically generates buttons for each animal in the array.
     var myButton = $("<button>");
     // Adds a class of animal-btn to the button.
     myButton.addClass("animal-btn");
@@ -80,7 +77,7 @@ $("#add-animal").on("click", function(event) {
    event.preventDefault();
    // Gets the input from the textbox.
    var animal = $("#animal-input").val().trim();
-   // Adds animal from the textbox to the array.
+   // Adds the input animal from the textbox to the array.
    animals.push(animal);
    // Calls renderButtons to display the buttons.
    renderButtons();
@@ -95,7 +92,7 @@ renderButtons();
 // Adds a click event listener for the image elements.    
 $(document).on("click","img", function() {
   // Creates the state variable with the state attribute value for 
-  // the image that was clicked.
+  // the image that is clicked.
   var state = $(this).attr("data-state");
   // If the clicked image state is still, updates its src attribute to its data-animate value, and sets the image data-state to animate. Otherwise, sets its src attribute to the data-still value, and sets the image data-state to still.
   if (state === "still") {
